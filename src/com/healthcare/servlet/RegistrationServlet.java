c9a9ed9826e187710442bc1e2368151d38f96e96
NipunEranda <namarasekara71@gmail.com>
2020-03-29 18:13:01 +0530

    user registration set

---
+++ src/com/healthcare/servlet/RegistrationServlet.java
@@ -0,0 +1,42 @@
+package com.healthcare.servlet;
+
+import java.util.HashMap;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.healthcare.service.DBManager;
+import com.healthcare.service.RegistrationService;
+import com.healthcare.service.RegistrationServiceImp;
+
+@Path("/user")
+public class RegistrationServlet {
+
+	RegistrationService registrationServiceObj = new RegistrationServiceImp();
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello() {
+		return "Hello";
+	}
+	
+	@Path("/registerUser")
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String registerUser(@FormParam("username")String username, @FormParam("email")String email, @FormParam("password")String password, @FormParam("mobileNumber")String mobileNumber, @FormParam("address")String address){
+		HashMap<String, String> h = registrationServiceObj.RegisterUser(username, email, password, mobileNumber, address);
+		
+		if(h.get("register").equalsIgnoreCase("success")) {
+			return "success";
+		}else {
+			return "fail";
+		}
+	}
+	
+}
